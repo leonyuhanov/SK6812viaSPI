eab921d9306d769351a69eb9733d0c690b93b0af
Elec Dash Tron <leon@elec-tron.org>
2017-09-16 20:51:28 +1000

    Add files via upload

---
+++ SK6812_RGBW_SPI/NeoViaSPI.cpp
@@ -0,0 +1,127 @@
+#include "NeoViaSPI.h"
+
+NeoViaSPI::NeoViaSPI(unsigned short int numLEDs)
+{  
+  _numLEDs = numLEDs;
+  _bytesPerLED = 4;
+  _LEDframeLength = _numLEDs*_bytesPerLED;
+  _NeoBitsframeLength = _LEDframeLength*4;
+  
+  LEDs = new byte[_LEDframeLength];
+  neoBits = new byte[_NeoBitsframeLength];
+
+  _blankValue = 0;
+  _blankBlock = 192;
+  
+  //set up test values;
+  _testValues[0][0] = 0;
+  _testValues[0][1] = 136;
+  _testValues[1][0] = 64;
+  _testValues[1][1] = 140;
+  _testValues[2][0] = 128;
+  _testValues[2][1] = 200;
+  _testValues[3][0] = 192;
+  _testValues[3][1] = 204;
+}
+
+void NeoViaSPI::setPixel(short int pixelIndex, byte *pixelColour)
+{
+  _counter = _bytesPerLED*pixelIndex;
+  LEDs[ _counter ] = pixelColour[1];
+  LEDs[ _counter + 1 ] = pixelColour[0];
+  LEDs[ _counter + 2 ] = pixelColour[2];
+}
+void NeoViaSPI::setRGBWPixel(short int pixelIndex, byte *pixelRGBWColour)
+{
+  _counter = _bytesPerLED*pixelIndex;
+  LEDs[ _counter ] = pixelRGBWColour[1];
+  LEDs[ _counter + 1 ] = pixelRGBWColour[0];
+  LEDs[ _counter + 2 ] = pixelRGBWColour[2];
+  LEDs[ _counter + 3 ] = pixelRGBWColour[3];
+}
+
+void NeoViaSPI::getPixel(short int pixelIndex, byte *pixelColour)
+{
+   _counter = _bytesPerLED*pixelIndex;
+   pixelColour[0] = LEDs[ _counter + 1 ];
+   pixelColour[1] = LEDs[ _counter ];
+   pixelColour[2] = LEDs[ _counter + 2 ];
+}
+void NeoViaSPI::getRGBWPixel(short int pixelIndex, byte *pixelRGBWColour)
+{
+   _counter = _bytesPerLED*pixelIndex;
+   pixelRGBWColour[0] = LEDs[ _counter + 1 ];
+   pixelRGBWColour[1] = LEDs[ _counter ];
+   pixelRGBWColour[2] = LEDs[ _counter + 2 ];
+   pixelRGBWColour[3] = LEDs[ _counter + 3 ];
+}
+
+void NeoViaSPI::encode()
+{
+  _nCounter = 0;
+  //Pixel Loop
+  for(_counter=0; _counter<_LEDframeLength; _counter+=_bytesPerLED)
+  {
+    //Colour Loop
+    for(_colCounter=0; _colCounter<_bytesPerLED; _colCounter++)
+    {
+      //Bit Loop
+      _tmpValue = LEDs[_counter+_colCounter];
+      for(_bitCounter=0; _bitCounter<4; _bitCounter++)
+      {
+        _blankValue = (_tmpValue & _blankBlock);
+        if( _blankValue == _testValues[0][0] )
+        {
+          neoBits[_nCounter] = _testValues[0][1];
+        }
+        else if( _blankValue == _testValues[1][0] )
+        {
+          neoBits[_nCounter] = _testValues[1][1];
+        }
+        else if( _blankValue == _testValues[2][0] )
+        {
+          neoBits[_nCounter] = _testValues[2][1];
+        }
+        else if( _blankValue == _testValues[3][0] )
+        {
+          neoBits[_nCounter] = _testValues[3][1];
+        }
+        _tmpValue = _tmpValue<<2;
+        _nCounter++;
+      }
+    }
+  }
+}
+
+unsigned int NeoViaSPI::saturation(byte *pixelRGBWColour)
+{
+    float low, high;
+    
+    // Find the smallest of all three parameters.
+    low = min(pixelRGBWColour[0], min(pixelRGBWColour[1], pixelRGBWColour[2]));
+    // Find the highest of all three parameters.
+    high = max(pixelRGBWColour[0], max(pixelRGBWColour[1], pixelRGBWColour[2]));
+    // The difference between the last two variables
+    // divided by the highest is the saturation.
+    return round(100 * ((high - low) / high));
+}
+
+byte NeoViaSPI::getWhite(byte *pixelRGBWColour) 
+{
+    return (255 - saturation(pixelRGBWColour)) / 255 * (pixelRGBWColour[0] + pixelRGBWColour[1] + pixelRGBWColour[2]) / 3;
+}
+
+void NeoViaSPI::rgbToRGBW()
+{
+  _nCounter = 0;
+  
+  for(_counter=0; _counter<_LEDframeLength; _counter+=_bytesPerLED)
+  {
+   _tmpCol[0] = LEDs[ _counter + 1 ];
+   _tmpCol[1] = LEDs[ _counter ];
+   _tmpCol[2] = LEDs[ _counter +2];
+   LEDs[ _counter + 3 ] = getWhite(_tmpCol); 
+  }
+    
+}
+
---
+++ SK6812_RGBW_SPI/SK6812_RGBW_SPI.ino
@@ -0,0 +1,199 @@
+/*
+      Coded for the NodeMCU module
+      //Pin out for module
+      D7  DATA  -   To SK6812 Data In
+*/
+#include <ESP8266WiFi.h>
+#include "SPI.h"
+#include "NeoViaSPI.h"
+
+//LED Stuff
+const short int numLeds = 10;
+const byte maxValue = 64;
+short int rainbowSize = maxValue*8;
+byte tempColour[3] = {0,0,0}, rgbwTempCol[4] = {0,0,0,0};
+short int pCnt=0;
+unsigned short int cIndex=0;
+
+NeoViaSPI leds = NeoViaSPI(numLeds);
+
+void setup()
+{
+  WiFi.mode(WIFI_OFF);
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setFrequency(3333333);
+  
+  //clear
+  rgbwTempCol[0] = 0;
+  rgbwTempCol[1] = 0;
+  rgbwTempCol[2] = 0;
+  rgbwTempCol[3] = 0;
+  for(pCnt=0; pCnt<numLeds; pCnt++)
+  {
+    leds.setRGBWPixel(pCnt, rgbwTempCol);
+  }
+  renderLEDs();
+
+}
+
+
+void renderLEDs()
+{
+  //leds.rgbToRGBW();
+  leds.encode();
+  SPI.writeBytes(leds.neoBits, leds._NeoBitsframeLength);
+  delayMicroseconds(80);
+}
+
+void loop()
+{
+
+  testPattern();
+//  getColour(cIndex%rainbowSize, tempColour);
+//  leds.setPixel(0, tempColour);
+//  for(pCnt=numLeds-1; pCnt>0; pCnt--)
+//  {
+//    leds.getPixel(pCnt-1, tempColour);
+//    leds.setPixel(pCnt, tempColour);
+//  }
+//  renderLEDs();
+//  cIndex+=2;
+//  delay(10);
+}
+
+void testPattern()
+{
+  //clear
+
+  rgbwTempCol[0] = 0;
+  rgbwTempCol[1] = 0;
+  rgbwTempCol[2] = 0;
+  rgbwTempCol[3] = 0;
+  for(pCnt=0; pCnt<numLeds; pCnt++)
+  {
+    leds.setRGBWPixel(pCnt, rgbwTempCol);
+  }
+  renderLEDs();
+  delay(1000);
+  
+  rgbwTempCol[0] = maxValue;
+  rgbwTempCol[1] = 0;
+  rgbwTempCol[2] = 0;
+  rgbwTempCol[3] = 0;
+  for(pCnt=0; pCnt<numLeds; pCnt++)
+  {
+    leds.setRGBWPixel(pCnt, rgbwTempCol);
+  }
+  renderLEDs();
+  delay(1000);
+
+  rgbwTempCol[0] = 0;
+  rgbwTempCol[1] = maxValue;
+  rgbwTempCol[2] = 0;
+  rgbwTempCol[3] = 0;
+  for(pCnt=0; pCnt<numLeds; pCnt++)
+  {
+    leds.setRGBWPixel(pCnt, rgbwTempCol);
+  }
+  renderLEDs();
+  delay(1000);
+  
+  rgbwTempCol[0] = 0;
+  rgbwTempCol[1] = 0;
+  rgbwTempCol[2] = maxValue;
+  rgbwTempCol[3] = 0;
+  for(pCnt=0; pCnt<numLeds; pCnt++)
+  {
+    leds.setRGBWPixel(pCnt, rgbwTempCol);
+  }
+  renderLEDs();
+  delay(1000);
+
+  rgbwTempCol[0] = maxValue;
+  rgbwTempCol[1] = maxValue;
+  rgbwTempCol[2] = maxValue;
+  rgbwTempCol[3] = 0;
+  for(pCnt=0; pCnt<numLeds; pCnt++)
+  {
+    leds.setRGBWPixel(pCnt, rgbwTempCol);
+  }
+  renderLEDs();
+  delay(1000);
+
+  rgbwTempCol[0] = 0;
+  rgbwTempCol[1] = 0;
+  rgbwTempCol[2] = 0;
+  rgbwTempCol[3] = maxValue;
+  for(pCnt=0; pCnt<numLeds; pCnt++)
+  {
+    leds.setRGBWPixel(pCnt, rgbwTempCol);
+  }
+  renderLEDs();
+  delay(1000);
+
+  rgbwTempCol[0] = maxValue;
+  rgbwTempCol[1] = maxValue;
+  rgbwTempCol[2] = maxValue;
+  rgbwTempCol[3] = maxValue;
+  for(pCnt=0; pCnt<numLeds; pCnt++)
+  {
+    leds.setRGBWPixel(pCnt, rgbwTempCol);
+  }
+  renderLEDs();
+  delay(1000);
+}
+
+void getColour(short int colourIndex, byte *currentColour)
+{
+
+ if( colourIndex>=0 && colourIndex<maxValue )
+ {
+    currentColour[0] = maxValue;
+    currentColour[1] = colourIndex;
+    currentColour[2] = 0; 
+ }
+ else if(colourIndex>=maxValue && colourIndex<maxValue*2)
+ {
+    currentColour[0] = maxValue-(colourIndex-maxValue);
+    currentColour[1] = maxValue;
+    currentColour[2] = 0; 
+ }
+ else if(colourIndex>=maxValue*2 && colourIndex<maxValue*3)
+ {
+    currentColour[0] = 0;
+    currentColour[1] = maxValue;
+    currentColour[2] = colourIndex-maxValue*2; 
+ }
+ else if(colourIndex>=maxValue*3 && colourIndex<maxValue*4)
+ {
+    currentColour[0] = 0;
+    currentColour[1] = maxValue-(colourIndex-maxValue*3);
+    currentColour[2] = maxValue; 
+ }
+ else if(colourIndex>=maxValue*4 && colourIndex<maxValue*5)
+ {
+    currentColour[0] = colourIndex-maxValue*4;
+    currentColour[1] = colourIndex-maxValue*4;
+    currentColour[2] = maxValue; 
+ }
+ else if(colourIndex>=maxValue*5 && colourIndex<maxValue*6)
+ {
+    currentColour[0] = maxValue;
+    currentColour[1] = maxValue-(colourIndex-maxValue*5);
+    currentColour[2] = maxValue-(colourIndex-maxValue*5); 
+ }
+ else if(colourIndex>=maxValue*6 && colourIndex<maxValue*7)
+ {
+    currentColour[0] = maxValue-(colourIndex-maxValue*6);
+    currentColour[1] = 0;
+    currentColour[2] = 0; 
+ }
+ else if(colourIndex>=maxValue*7 && colourIndex<maxValue*8)
+ {
+    currentColour[0] = (colourIndex-maxValue*7);
+    currentColour[1] = 0;
+    currentColour[2] = 0; 
+ }
+ return;
+}
