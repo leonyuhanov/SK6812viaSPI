809df458c9245cb40cf2f95637ff0b6c9c72d1f0
Elec Dash Tron <leon@elec-tron.org>
2017-09-16 20:52:04 +1000

    Add files via upload

---
+++ SK6812_RGBW_SPI/NeoViaSPI.h
@@ -0,0 +1,40 @@
+#ifndef NeoViaSPI_h
+#define NeoViaSPI_h
+#include "Arduino.h"
+
+class NeoViaSPI
+{
+  public:
+    NeoViaSPI(unsigned short int numLEDs);
+    void setPixel(short int pixelIndex, byte *pixelColour);
+    void setRGBWPixel(short int pixelIndex, byte *pixelRGBWColour);
+    void getPixel(short int pixelIndex, byte *pixelColour);
+    void getRGBWPixel(short int pixelIndex, byte *pixelRGBWColour);
+    void encode();
+    
+    unsigned int saturation(byte *pixelRGBWColour);
+    byte getWhite(byte *pixelRGBWColour);
+    void rgbToRGBW(); 
+    
+    byte* LEDs;
+    byte* neoBits;
+    
+    unsigned short int _rainbowSize;
+    unsigned short int _LEDframeLength;
+    unsigned short int _NeoBitsframeLength;
+    unsigned short int _numLEDs;
+    unsigned short int _counter;
+    unsigned short int _nCounter;
+    byte _colCounter;
+    byte _bitCounter;
+    
+  private:
+    byte _blankValue;
+    byte _tmpValue;
+    byte _blankBlock;
+    byte _testValues[4][2];
+    byte _bytesPerLED;
+    byte _tmpCol[3];
+};
+
+#endif
